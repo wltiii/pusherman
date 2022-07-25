cb165f0990c778178cb1897b1c74c4aa157a154d
William.Turner@medxoom.com <William.Turner@medxoom.com>
2022-07-24 21:57:53 -0500

    Prescription: WIP: reverted freezed changes - moving toward roll your own de/serialization to get the rest of the app works.

--- lib/domain/core/models/types/treatment/prescription.dart
+++ lib/domain/core/models/types/treatment/prescription.dart
@@ -7,7 +7,7 @@ import 'package:pusherman/domain/core/models/value_objects/whole_number.dart';
 
 import '../type_defs.dart';
 
-part 'prescription.freezed.dart';
+// part 'prescription.freezed.dart';
 part 'prescription.g.dart';
 
 /// A [Prescription] is a medication used to treat an ailment. It
@@ -23,67 +23,66 @@ part 'prescription.g.dart';
 // TODO(wltiii): Perhaps extend from a common abstraction. NOTE:
 // TODO(wltiii): I expect them to diverge in the future.
 @JsonSerializable(explicitToJson: true)
-@freezed
-class Prescription extends Treatment with _$Prescription {
-  const Prescription._();
+// @freezed
+class Prescription extends Treatment {
+// class Prescription extends Treatment with _$Prescription {
+//   const Prescription._();
+//
+//   const factory Prescription(
+//     Dependent dependent,
+//     CareGiver? caregiver,
+//     PrescriptionDescription description,
+//     PrescriptionDirections directions,
+//     PrescriptionRefillQuantity prescriptionRefillQuantity,
+//     PrescriptionOnHandQuantity prescriptionOnHandQuantity,
+//   ) = _Prescription;
 
-  const factory Prescription(
+  Prescription(
     Dependent dependent,
     CareGiver? caregiver,
     PrescriptionDescription description,
     PrescriptionDirections directions,
     PrescriptionRefillQuantity prescriptionRefillQuantity,
     PrescriptionOnHandQuantity prescriptionOnHandQuantity,
-  ) = _Prescription;
+  ) : super(
+          dependent,
+          caregiver,
+          description,
+          directions,
+        ) {
+    _prescriptionRefillQuantity = prescriptionRefillQuantity;
+    _prescriptionOnHandQuantity = prescriptionOnHandQuantity;
+  }
 
-  // Prescription(
-  //   Dependent dependent,
-  //   CareGiver? caregiver,
-  //   PrescriptionDescription description,
-  //   PrescriptionDirections directions,
-  //   PrescriptionRefillQuantity prescriptionRefillQuantity,
-  //   PrescriptionOnHandQuantity prescriptionOnHandQuantity,
-  // ) : super(
-  //         dependent,
-  //         caregiver,
-  //         description,
-  //         directions,
-  //       ) {
-  //   _prescriptionRefillQuantity = prescriptionRefillQuantity;
-  //   _prescriptionOnHandQuantity = prescriptionOnHandQuantity;
-  // }
-  //
-  // late final PrescriptionRefillQuantity _prescriptionRefillQuantity;
-  // late final PrescriptionOnHandQuantity _prescriptionOnHandQuantity;
+  late final PrescriptionRefillQuantity _prescriptionRefillQuantity;
+  late final PrescriptionOnHandQuantity _prescriptionOnHandQuantity;
 
-  // int get refillQuantity => _prescriptionRefillQuantity.value;
-  int get refillQuantity => prescriptionRefillQuantity.value;
+  int get refillQuantity => _prescriptionRefillQuantity.value;
+  // int get refillQuantity => prescriptionRefillQuantity.value;
 
-  // int get onHandQuantity => _prescriptionOnHandQuantity.value;
-  int get onHandQuantity => prescriptionOnHandQuantity.value;
+  int get onHandQuantity => _prescriptionOnHandQuantity.value;
+  // int get onHandQuantity => prescriptionOnHandQuantity.value;
 
   /// Connect the generated [_$PrescriptionFromJson] function to the `fromJson`
   /// factory.
   factory Prescription.fromJson(Json json) =>
       _$PrescriptionFromJson(json);
 
-  // /// Connect the generated [_$PrescriptionToJson] function to the `toJson` method.
-  // Map<String, dynamic> toJson() => _$PrescriptionToJson(this);
-  //
-  // //TODO(wltiii): get props seems to beg to be generated
-  // @override
-  // List get props => super.props
-  //   ..addAll(
-  //     <dynamic>[
-  //       _prescriptionRefillQuantity,
-  //       _prescriptionOnHandQuantity,
-  //     ],
-  //   )
-  //   ..toList();
-  //
-  // //TODO(wltiii): most definitely this should be generated
-  // @override
-  // bool get stringify => true;
+  /// Connect the generated [_$PrescriptionToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$PrescriptionToJson(this);
+
+  @override
+  List get props => super.props
+    ..addAll(
+      <dynamic>[
+        _prescriptionRefillQuantity,
+        _prescriptionOnHandQuantity,
+      ],
+    )
+    ..toList();
+
+  @override
+  bool get stringify => true;
 }
 
 class PrescriptionDescription extends TreatmentDescription {
