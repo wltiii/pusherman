b689977d175c3abcaeaee1a533323d9bf006ef7b
wltiii <myX2bear>
2022-07-31 16:25:05 -0500

    WIP: building out JSON serialization for Organizer.

--- lib/domain/core/models/types/treatment_containers/compartment.dart
+++ lib/domain/core/models/types/treatment_containers/compartment.dart
@@ -1,24 +1,72 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:pusherman/domain/core/error/exceptions.dart';
 import 'package:pusherman/domain/core/models/types/treatment/treatment.dart';
-import 'package:pusherman/domain/core/models/types/type_defs.dart';
 import 'package:pusherman/domain/core/models/value_objects/exception_message.dart';
 
-part 'compartment.freezed.dart';
-part 'compartment.g.dart';
+// part 'compartment.g.dart';
 
-@JsonSerializable(explicitToJson: true)
-@freezed
-class Compartment with _$Compartment {
-  const Compartment._();
-
-  factory Compartment(
+// @JsonSerializable(explicitToJson: true)
+// class Compartment extends Equatable {
+class Compartment {
+  Compartment(
     List<Treatment> treatments,
-  ) {
-    // _treatments = validate(treatments);
-    treatments = validate(treatments);
-  }
+  ) : _treatments = validate(treatments);
+
+// factory Compartment.fromJson(Map<String, dynamic> json) {
+//   var somePills = json['pills'].map((pill) => Pill.fromJson(pill))
+//       .toList()
+//       .cast<Pill>();
+//
+//   var pillSet = TreatmentBox(
+//     _name: json['name'],
+//     _frequency: json['frequency'] ?? '',
+//     _treatments: somePills,
+//   );
+//
+//   return pillSet;
+//
+// }
+
+  // /// Connect the generated [_$CompartmentFromJson] function to the `fromJson`
+  // /// factory.
+  // factory Compartment.fromJson(Json json) => _$CompartmentFromJson(json);
+
+  // /// Connect the generated [_$CompartmentToJson] function to the `toJson` method.
+  // Map<String, dynamic> toJson() => _$CompartmentToJson(this);
+
+  late final Set<Treatment> _treatments;
+
+  // List<Treatment> get list => _treatments.toList();
+  List<Treatment> get list => _treatments.toList();
+
+  String get dependentName => list.isNotEmpty ? list[0].dependent.name : '';
+
+  String get caregiverName => list.isNotEmpty ? list[0].caregiver.name : '';
+
+  @override
+  List<Object> get props => [_treatments];
+
+  @override
+  bool get stringify => true;
+
+  /// Connect the generated [_$CompartmentFromJson] function to the `fromJson`
+  /// factory.
+// factory Compartment.fromJson(Json json) => _$CompartmentFromJson(json);
+
+  // Json toJson() {
+  //   return {
+  //     "compartments": [
+  //       {
+  //         "runtimeType": "Prescription",
+  //         "caregiver": "Bill",
+  //         "dependent": "Coda",
+  //         "description": "A Test Prescription",
+  //         "directions": "Take frequently",
+  //         "prescriptionRefillQuantity": 30,
+  //         "prescriptionOnHandQuantity": 17
+  //       }
+  //     ]
+  //   };
+  // }
 
   static Set<Treatment> validate(
     List<Treatment> treatments,
@@ -53,47 +101,4 @@ class Compartment with _$Compartment {
 
     return treatments.toSet();
   }
-
-  // late final Set<Treatment> _treatments;
-
-  // List<Treatment> get list => _treatments.toList();
-  List<Treatment> get list => treatments.toList();
-
-  String get dependentName =>
-      list.isNotEmpty ? list[0].dependent.name : '';
-
-  String get caregiverName =>
-      list.isNotEmpty ? list[0].caregiver.name : '';
-
-// @override
-// List<Object> get props => [_treatments];
-
-// @override
-// bool get stringify => true;
-
-  /// Connect the generated [_$CompartmentFromJson] function to the `fromJson`
-  /// factory.
-  factory Compartment.fromJson(Json json) =>
-      _$CompartmentFromJson(json);
-
-// factory TreatmentBox.fromJson(Map<String, dynamic> json) {
-//   var somePills = json['pills'].map((pill) => Pill.fromJson(pill))
-//       .toList()
-//       .cast<Pill>();
-//
-//   var pillSet = TreatmentBox(
-//     _name: json['name'],
-//     _frequency: json['frequency'] ?? '',
-//     _treatments: somePills,
-//   );
-//
-//   return pillSet;
-//
-// }
-
-// Map<String, dynamic> toJson() {
-//   return {
-//     "name": _name,
-//   };
-// }
 }
