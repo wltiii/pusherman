c61c185a784f1eb7b2cc79dadc6ad765ceab9b39
wltiii <myX2bear>
2022-12-01 12:15:50 -0600

    user.dart: replaced core lib imports with unrepresentable_state/unrepresentable_state failures: unrepresentable_state/unrepresentable_state

--- lib/domain/core/models/types/auth/user.dart
+++ lib/domain/core/models/types/auth/user.dart
@@ -1,51 +1,45 @@
 import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
-import 'package:pusherman/domain/core/error/exceptions.dart';
-import 'package:pusherman/domain/core/models/value_objects/exception_message.dart';
-import 'package:pusherman/domain/core/models/value_objects/non_empty_string.dart';
 import 'package:pusherman/domain/core/models/types/type_defs.dart';
+import 'package:unrepresentable_state/unrepresentable_state.dart';
 
 abstract class User extends Equatable {
-  const User(this._id, this._name,);
+  const User(
+    this._id,
+    this._name,
+  );
 
   static User fromJson(String source) {
     final map = json.decode(source) as Map<String, Object?>;
 
-    final type =
-        map['runtimeType'] is String ? map['runtimeType'] as String : '';
+    final type = map['runtimeType'] is String ? map['runtimeType'] as String : '';
 
     switch (type) {
       case 'Dependent':
         return Dependent(
-          UserId(map['id'] as String),
+          LoginId(map['id'] as String),
           UserName(map['userName'] as String),
         );
       case 'CareGiver':
         return CareGiver(
-          UserId(map['id'] as String),
+          LoginId(map['id'] as String),
           UserName(map['userName'] as String),
         );
       case 'CareProvider':
         return CareProvider(
-          UserId(map['id'] as String),
+          LoginId(map['id'] as String),
           UserName(map['userName'] as String),
         );
     }
-    //TODO is there a test for this?
-    throw ValueException(
-        ExceptionMessage('Unrecognized value $type.')
-    );
+    throw ValueException(ExceptionMessage('Unrecognized value $type.'));
   }
 
   Json toJson() {
-    return {
-      'runtimeType': this.runtimeType.toString(),
-      'id': id,
-      'userName': name
-    };
+    return {'runtimeType': this.runtimeType.toString(), 'id': id, 'userName': name};
   }
 
-  final UserId _id;
+  final LoginId _id;
   final UserName _name;
 
   String get id => _id.value;
@@ -60,7 +54,7 @@ abstract class User extends Equatable {
 }
 
 class Dependent extends User {
-  const Dependent(UserId id, UserName userName) : super(id, userName);
+  const Dependent(LoginId id, UserName userName) : super(id, userName);
 
   factory Dependent.fromJson(String source) {
     return User.fromJson(source) as Dependent;
@@ -68,7 +62,7 @@ class Dependent extends User {
 }
 
 class CareGiver extends User {
-  const CareGiver(UserId id, UserName userName) : super(id, userName);
+  const CareGiver(LoginId id, UserName userName) : super(id, userName);
 
   factory CareGiver.fromJson(String source) {
     return User.fromJson(source) as CareGiver;
@@ -76,33 +70,11 @@ class CareGiver extends User {
 }
 
 class CareProvider extends User {
-  const CareProvider(UserId id, UserName userName) : super(id, userName);
+  const CareProvider(LoginId id, UserName userName) : super(id, userName);
 
   factory CareProvider.fromJson(String source) {
     return User.fromJson(source) as CareProvider;
   }
-}
-
-/// [UserId] is a unique identifier of a user.
-///
-/// It is a [NonEmptyString], thus  must not be empty. Instantiating with an
-/// empty String will throw a [ValueException].
-///
-// TODO(wltiii): allow empty and generate a unique id???
-// TODO(wltiii): or use StoreMetaData???
-class UserId extends NonEmptyString {
-  UserId(String value)
-      : super(
-    value,
-    validators: [
-          (String value) => {
-        if (value.trimRight().isEmpty)
-          throw ValueException(
-            ExceptionMessage('User id must not be empty.'),
-          ),
-      },
-    ],
-  );
 }
 
 /// [UserName] is a user friendly name associated with the given user.
@@ -114,14 +86,14 @@ class UserId extends NonEmptyString {
 class UserName extends NonEmptyString {
   UserName(String value)
       : super(
-    value,
-    validators: [
+          value,
+          validators: [
             (String value) => {
                   if (value.trimRight().isEmpty)
-          throw ValueException(
-            ExceptionMessage('User name must not be empty.'),
-          ),
-      },
-    ],
-  );
+                    throw ValueException(
+                      ExceptionMessage('User name must not be empty.'),
+                    ),
+                },
+          ],
+        );
 }
