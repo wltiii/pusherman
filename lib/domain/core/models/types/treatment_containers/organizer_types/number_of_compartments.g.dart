4b41fd7e0cd9060f3d1c3ca0eb5755267af9cc48
wltiii <myX2bear>
2022-12-04 10:25:08 -0600

    added generated file

---
+++ lib/domain/core/models/types/treatment_containers/organizer_types/number_of_compartments.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'number_of_compartments.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NumberOfCompartments _$NumberOfCompartmentsFromJson(
+        Map<String, dynamic> json) =>
+    NumberOfCompartments(
+      json['value'] as int,
+    );
+
+Map<String, dynamic> _$NumberOfCompartmentsToJson(
+        NumberOfCompartments instance) =>
+    <String, dynamic>{
+      'value': instance.value,
+    };
